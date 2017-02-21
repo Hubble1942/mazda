0ea4624041fd3b82d038a5b17f1672978cc795b2
Christian Ewald <christian@ewald.li>
2017-02-21 21:35:25 +0100

    Add sublime settings

--- setup.sh
+++ setup.sh
@@ -3,11 +3,8 @@
 # get this scripts location
 ROOT_DIR=$( cd "$( dirname "$0" )" && pwd )
 
-#ln -s -f $ROOT_DIR/home/.bashrc ~/.bashrc
 
 # enable * matches dot-files as well
 shopt -s dotglob
-for FILE_PATH in $ROOT_DIR/home/*; do
-    FILE_NAME=$(basename "$FILE_PATH")
-    ln -s -f $FILE_PATH ~/$FILE_NAME
-done
+
+cp -asf $ROOT_DIR/home/* ~/
