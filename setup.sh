081e777fe7ad8758aeb3f9a3e111b89c349207f0
Christian Ewald <christian@ewald.li>
2017-02-22 21:49:48 +0100

    Install some default packages

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function ensure {
+    hash $1 2>/dev/null || { sudo apt-get -y install $2 $3 $4 $5; }
+}
+
+# install some packages if they're not already there
+ensure hg mercurial
+ensure thg tortoisehg python-iniparse tortoisehg-nautilus
+ensure subl sublime-text
+
 # get this scripts location
 ROOT_DIR=$( cd "$( dirname "$0" )" && pwd )
 
