dde49cea4f3507b8bd51e50addf9720a5c293370
Christian Ewald <vfr750rc24-bitbucket@yahoo.com>
2020-10-08 12:30:10 +0200

    Get rid of mercurial

--- setup.sh
+++ setup.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-function ensure {
-    hash $1 2>/dev/null || { sudo apt-get -y install $2 $3 $4 $5; }
-}
-
-# install some packages if they're not already there
-ensure hg mercurial
-
 # get this scripts location
 ROOT_DIR=$( cd "$( dirname "$0" )" && pwd )
-
 
 # enable * matches dot-files as well
 shopt -s dotglob
